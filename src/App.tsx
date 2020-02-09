30b0b259af387fa6dee9eb5614c190111ded5959
nnyam3831 <nnyam3831@naver.com>
2020-02-09 17:51:24 +0900

    useReducer 사용하기

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,10 @@ import "./App.css";
 import Greetings from "./Greeting";
 import Counter from "./Counter";
 import MyForm from "./MyForm";
+import ReducerSample from "./ReducerSample";
 
 const App = () => {
-  const onSubmit = (form: { name: string; description: string }) => {
-    console.log(form);
-  };
-  return <MyForm onSubmit={onSubmit} />;
+  return <ReducerSample />;
 };
 
 export default App;
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,22 +1,31 @@
-// useState는 generic을 사용하지 않아도 type을 유추할 수 있기 때문에 굳이 사용하지 않아도 됨
-// But Type이 null 이거나 객체 or 배열일 때 Generic을 명시하는 게 효율적임
+// useReducer로 카운터 구현
 
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+
+type Action = { type: "INCREASE" } | { type: "DECREASE" };
+
+const reducer = (state: number, action: Action): number => {
+  switch (action.type) {
+    case "INCREASE":
+      return state + 1;
+    case "DECREASE":
+      return state - 1;
+    default:
+      throw new Error("unhandled action");
+  }
+};
 
 const Counter = () => {
-  const [count, setCount] = useState<number>(0);
-  const onIncrease = () => setCount(count + 1);
-  const onDecrease = () => setCount(count - 1);
+  const [count, dispatch] = useReducer(reducer, 0);
+  const onIncrease = () => dispatch({ type: "INCREASE" });
+  const onDecrease = () => dispatch({ type: "DECREASE" });
 
-  // 요런식으로
-  type Todo = { id: number; text: string; done: boolean };
-  const [todos, setTodos] = useState<Todo[]>([]);
   return (
     <div>
       <h1>{count}</h1>
       <div>
         <button onClick={onIncrease}>+1</button>
-        <button onClick={onDecrease}>-1</button>
+        <button onClick={onDecrease}>+1</button>
       </div>
     </div>
   );
